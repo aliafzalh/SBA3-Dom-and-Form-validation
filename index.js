0a3ae5110200add64a198daae652234bdc87b67c
aliafzalh <aliafzalh@gmail.com>
2025-05-19 17:26:42 -0400

    HTML form validation added to the index file

---
+++ index.js
@@ -0,0 +1,35 @@
+ document.addEventListener(`DOMContentLoaded`,()=>{
+const title = document.getElementById(`title`)
+console.log(title)
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+
+const bookForm = document.getElementById(`book-form`)
+console.log(bookForm)
+const allinputs = document.querySelectorAll(`input`)
+console.log(allinputs)
+
+const btn = document.querySelector(`#submitbtn`)
+console.log(btn)
+
+
+
+// btn.addEventListener('click', () => {
+//   alert('Book has been added to the list!!');
+// });
+
+btn.addEventListener(`click`,()=>{
+    // event.preventDefault();
+    if(!title.value || !author.value || !pages.value){
+        alert(`Plese fill in all the fields correctly.`)
+    }
+    else{
+       alert(`Book has been added to the list!!`)
+    }
+})
+
+ })
+
+// const alertbtn = document.querySelectorAll(`title ||autor || pages`)
+// alertbtn.addEventListener(alert(`Please fill in all the fields correctly.`))
+// return;
