a2515304a95a48cff618f346cf21d332a5886312
aliafzalh <aliafzalh@gmail.com>
2025-05-21 23:41:41 -0400

    Updated Code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
  document.addEventListener(`DOMContentLoaded`,()=>{
 const title = document.getElementById(`title`)
-console.log(title)
+// console.log(title)
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 
 const bookForm = document.getElementById(`book-form`)
-console.log(bookForm)
+// console.log(bookForm)
 const allinputs = document.querySelectorAll(`input`)
-console.log(allinputs)
+// console.log(allinputs)
 
 const btn = document.querySelector(`#submitbtn`)
-console.log(btn)
+// console.log(btn)
 
 
 
@@ -29,4 +29,57 @@ btn.addEventListener(`click`,()=>{
 })
 
  })
+const div = document.createElement(`div`)
+div.id = `list`
+div.textContent= `This is the list of book that I want to read in future. `
+document.body.appendChild(div)
 
+const ol = document.createElement(`ol`)
+ol.classList.add(`list`)
+div.appendChild(ol)
+// const li = document.createElement(`li`)
+// ul.appendChild(li)
+
+for (let i = 1; i<=3;i++){     //Adding list of the intem in the div.ul
+    const li = document.createElement(`li`)
+    li.textContent = `Book:`
+    ol.appendChild(li)
+}
+const firstbook = ol.firstElementChild;
+console.log(`First book:`, firstbook.textContent)
+
+const lastbook = ol.lastElementChild;
+console.log(`Last Book:`, lastbook.textContent)
+
+const parentOl = lastbook.parentNode;
+console.log(`Parent of last book:`, parentOl.tagName);
+
+
+const item = document.querySelectorAll(`#list li`)
+item.forEach((list)=>{
+    console.table(list.textContent)
+})
+
+const section = document.createElement(`section`)
+section.id = `sec`
+const main = document.querySelector(`main`)
+main.appendChild(section)
+
+const message = document.createElement(`h2`)
+message.id = `message`
+message.textContent = `📖Welcome to my book list!!`;
+section.appendChild(message)
+
+const change_msg =document.createElement(`button`)
+change_msg.id=`change`
+change_msg.innerHTML=`Change Message`
+
+section.appendChild(change_msg);
+
+
+    document.getElementById('change').addEventListener('click', () => {
+    const msg = document.getElementById('message');
+    msg.textContent = `📚 Let's add some awesome books!!`;
+
+  });
+  
